fef4cdddf71b5512d7b197039132c16ccbba52cd
Grishanov_Dmitry <grishanov.ds@gmail.com>
2019-04-22 23:40:29 +0300

    Started do promo

---
+++ src/components/molecules/Promo/Promo.stories.tsx
@@ -0,0 +1,9 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+import { Promo } from 'molecules';
+
+storiesOf('Molecules', module)
+.add('Promo', () => (
+  <Promo/>
+))
