32f332ce0e80e87392a06d0fe9ff48a1bce4d74d
Dmitry-Grishanov <d.grishanov@arateg.by>
2019-04-22 10:25:56 +0300

    Started do promo

---
+++ src/components/molecules/Promo/Promo.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface IProps {
+  
+}
+
+export const Promo = (
+  {  }: IProps
+) => (
+<Wrapper>
+  Text
+</Wrapper>
+);
+
+const Wrapper = styled.div`
+
+`;
+ 
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,3 +1,5 @@
 export { Header } from "./Header";
 export { MenuDesktop } from "./MenuDesktop";
 export { Slider } from "./Slider";
+export { Promo } from './Promo';
+ 
