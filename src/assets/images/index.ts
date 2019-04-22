c55a3b2ca96456faec00bf791bc8fa8fd3ff4d81
Grishanov_Dmitry <grishanov.ds@gmail.com>
2019-04-22 23:18:44 +0300

    Jackpot list are ready. All versions

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,7 +4,9 @@ import * as hearts from './hearts.png';
 import * as spades from './spades.png';
 import * as diamonds from './diamonds.png';
 import * as clubs from './clubs.png';
+import * as jackpotcardsLogo from './jackpotcards-logo.png';
+import * as jackpotcardsLogoMobi from './jackpotcards-logo-mobile.png';
 import { banner_1, banner_2 } from './banners';
 
 export { mainLogo, menuIcon, banner_1, banner_2, hearts, spades,
-  diamonds, clubs };
+  diamonds, clubs, jackpotcardsLogo, jackpotcardsLogoMobi };
--- src/components/molecules/JackpotCardsList/JackpotCardsList.tsx
+++ src/components/molecules/JackpotCardsList/JackpotCardsList.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { jackpotcardsLogo , jackpotcardsLogoMobi } from 'images'
+
 interface ICard {
   image: HTMLImageElement;
   jackpotValue: number;
@@ -16,6 +18,7 @@ export const JackpotCardsList = (
   { cardJackpotData }: IProps
 ) => (
 <Wrapper>
+  <Logo image={jackpotcardsLogo}/>
   {
     cardJackpotData.map((card: ICard, i) =>
       <CardJeckpot key={i} jackpotValue={card.jackpotValue} image={card.image}/> )
@@ -24,6 +27,23 @@ export const JackpotCardsList = (
 );
 
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
 
+const Logo = styled.div<{image: HTMLImageElement}>`
+  width: 113px;
+  height: 53px;
+  background: ${({image}) => `url(${image}) no-repeat`};
+  background-position-y: center;
+  background-size: contain;
+  @media screen and (max-width: 992px) {
+    width: 100%;
+    margin-bottom: 10px;
+  }
+  @media screen and (max-width: 580px) {
+    background: url(${jackpotcardsLogoMobi}) center no-repeat;
+  }
 `;
  
