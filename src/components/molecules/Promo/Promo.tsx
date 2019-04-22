8a6773a468bea843de8faaedc5eb480ba6d0c036
Grishanov_Dmitry <grishanov.ds@gmail.com>
2019-04-22 23:40:29 +0300

    Started do promo

--- src/components/molecules/Promo/Promo.tsx
+++ src/components/molecules/Promo/Promo.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-interface IProps {
-  
-}
+import { colors } from 'context';
+import { banner_1 } from "images";
 
-export const Promo = (
-  {  }: IProps
-) => (
+export const Promo = () => (
 <Wrapper>
-  Text
+  <Banner image={banner_1}/>
 </Wrapper>
 );
 
 const Wrapper = styled.div`
+  border-top: 40px solid ${colors.darkSilver};
+  border-bottom: 40px solid ${colors.darkSilver};
+`;
 
+const Banner = styled.div<{image: HTMLImageElement}>`
+  width: 300px;
+  height: 300px;
+  background: url(image);
 `;
  
