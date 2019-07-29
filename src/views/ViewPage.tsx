d8ff27c03226027ca68b0ba698f6537993463457
Dmitry-Grishanov <d.grishanov@arateg.by>
2019-07-29 10:36:56 +0300

    Turned on the jeckpots

--- src/views/ViewPage.tsx
+++ src/views/ViewPage.tsx
@@ -34,27 +34,27 @@ export class ViewPage extends React.Component<IProps> {
     changeHeaderNav(value);
   }
 
-  // public componentDidMount() {
-  //   const { loadJackpots, updateJackpots } = this.props;
+  public componentDidMount() {
+    const { loadJackpots, updateJackpots } = this.props;
 
-  //   loadJackpots();
-  //   setInterval(() => {
-  //     updateJackpots();
-  //   }, 7000);
-  //   const { saveNewTime } = this.props;
+    loadJackpots();
+    setInterval(() => {
+      updateJackpots();
+    }, 7000);
+    const { saveNewTime } = this.props;
 
-  //   setInterval(() => {
-  //     const date = new Date();
+    setInterval(() => {
+      const date = new Date();
 
-  //     const hours = date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`
-  //     const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`
-  //     const seconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : `${date.getSeconds()}`
+      const hours = date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`
+      const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`
+      const seconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : `${date.getSeconds()}`
 
-  //     const newTime = `${hours}:${minutes}:${seconds}`;
+      const newTime = `${hours}:${minutes}:${seconds}`;
 
-  //     saveNewTime(newTime);
-  //   }, 1000)
-  // }
+      saveNewTime(newTime);
+    }, 1000)
+  }
 
   public onMenuClick =(value: string) => {
     const { changeMenuNav } = this.props;
