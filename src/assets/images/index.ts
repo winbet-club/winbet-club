cf8f536d15bea86a9678d0685bb8e0f48f59abd1
dmitry-grishanov <grishanov.ds@gmail.com>
2020-03-27 19:46:02 +0300

    Added documentation-2. Change favicon

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,35 +1,65 @@
-import * as mainLogo from './main-logo.png';
-import * as menuIcon from './menu-icon.svg';
-import * as hearts from './hearts.png';
-import * as spades from './spades.png';
-import * as diamonds from './diamonds.png';
-import * as clubs from './clubs.png';
-import * as jackpotcardsLogo from './jackpotcards-logo.png';
-import * as jackpotcardsLogoMobi from './jackpotcards-logo-mobile.png';
-import * as aboutBg from './about-bg.png';
-import * as careeraBg from './career-bg.png';
-import * as sliderBg from './sliderBg.jpg';
-import { banner_1, banner_2, banner_3 } from './banners';
-import {  promo_1, promo_2 } from './promo';
-import { rusFlag, usaFlag } from './flags';
-import * as russiyanova_1 from './casinos/russiyanova/russiyanova-1.jpg';
-import * as russiyanova_2 from './casinos/russiyanova/russiyanova-2.jpg';
-import * as russiyanova_3 from './casinos/russiyanova/russiyanova-3.jpg';
-import * as russiyanova_4 from './casinos/russiyanova/russiyanova-4.jpg';
-import * as russiyanova_5 from './casinos/russiyanova/russiyanova-5.jpg';
-import * as bogdanovicha_1 from './casinos/bogdanovicha/bogdanovicha_1.jpg';
-import * as bogdanovicha_2 from './casinos/bogdanovicha/bogdanovicha_2.jpg';
-import * as bogdanovicha_3 from './casinos/bogdanovicha/bogdanovicha_3.jpg';
-import * as bogdanovicha_4 from './casinos/bogdanovicha/bogdanovicha_4.jpg';
-import * as bogdanovicha_5 from './casinos/bogdanovicha/bogdanovicha_5.jpg';
-import * as nemiga_1 from './casinos/nemiga/nemiga_1.jpg';
-import * as nemiga_2 from './casinos/nemiga/nemiga_2.jpg';
-import * as nemiga_3 from './casinos/nemiga/nemiga_3.jpg';
-import * as nemiga_4 from './casinos/nemiga/nemiga_4.jpg';
-import * as nemiga_5 from './casinos/nemiga/nemiga_5.jpg';
+import * as mainLogo from "./main-logo.png";
+import * as menuIcon from "./menu-icon.svg";
+import * as hearts from "./hearts.png";
+import * as spades from "./spades.png";
+import * as diamonds from "./diamonds.png";
+import * as clubs from "./clubs.png";
+import * as jackpotcardsLogo from "./jackpotcards-logo.png";
+import * as jackpotcardsLogoMobi from "./jackpotcards-logo-mobile.png";
+import * as aboutBg from "./about-bg.png";
+import * as careeraBg from "./career-bg.png";
+import * as sliderBg from "./sliderBg.jpg";
+import { banner_1, banner_2, banner_3 } from "./banners";
+import { promo_1, promo_2 } from "./promo";
+import { rusFlag, usaFlag } from "./flags";
+import * as russiyanova_1 from "./casinos/russiyanova/russiyanova-1.jpg";
+import * as russiyanova_2 from "./casinos/russiyanova/russiyanova-2.jpg";
+import * as russiyanova_3 from "./casinos/russiyanova/russiyanova-3.jpg";
+import * as russiyanova_4 from "./casinos/russiyanova/russiyanova-4.jpg";
+import * as russiyanova_5 from "./casinos/russiyanova/russiyanova-5.jpg";
+import * as bogdanovicha_1 from "./casinos/bogdanovicha/bogdanovicha_1.jpg";
+import * as bogdanovicha_2 from "./casinos/bogdanovicha/bogdanovicha_2.jpg";
+import * as bogdanovicha_3 from "./casinos/bogdanovicha/bogdanovicha_3.jpg";
+import * as bogdanovicha_4 from "./casinos/bogdanovicha/bogdanovicha_4.jpg";
+import * as bogdanovicha_5 from "./casinos/bogdanovicha/bogdanovicha_5.jpg";
+import * as nemiga_1 from "./casinos/nemiga/nemiga_1.jpg";
+import * as nemiga_2 from "./casinos/nemiga/nemiga_2.jpg";
+import * as nemiga_3 from "./casinos/nemiga/nemiga_3.jpg";
+import * as nemiga_4 from "./casinos/nemiga/nemiga_4.jpg";
+import * as nemiga_5 from "./casinos/nemiga/nemiga_5.jpg";
 
-export { mainLogo, menuIcon, banner_1, banner_2, banner_3, hearts, spades,diamonds, clubs, jackpotcardsLogo, 
-  jackpotcardsLogoMobi, promo_1, promo_2, careeraBg, aboutBg, rusFlag, usaFlag, sliderBg,
-  russiyanova_1, russiyanova_2, russiyanova_3, russiyanova_4, russiyanova_5,
-  bogdanovicha_1, bogdanovicha_2, bogdanovicha_3, bogdanovicha_4, bogdanovicha_5,
-  nemiga_1, nemiga_2, nemiga_3, nemiga_4, nemiga_5 };
+export {
+  mainLogo,
+  menuIcon,
+  banner_1,
+  banner_2,
+  banner_3,
+  hearts,
+  spades,
+  diamonds,
+  clubs,
+  jackpotcardsLogo,
+  jackpotcardsLogoMobi,
+  promo_1,
+  promo_2,
+  careeraBg,
+  aboutBg,
+  rusFlag,
+  usaFlag,
+  sliderBg,
+  russiyanova_1,
+  russiyanova_2,
+  russiyanova_3,
+  russiyanova_4,
+  russiyanova_5,
+  bogdanovicha_1,
+  bogdanovicha_2,
+  bogdanovicha_3,
+  bogdanovicha_4,
+  bogdanovicha_5,
+  nemiga_1,
+  nemiga_2,
+  nemiga_3,
+  nemiga_4,
+  nemiga_5
+};
